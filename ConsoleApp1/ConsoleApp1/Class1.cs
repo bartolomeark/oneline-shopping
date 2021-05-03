5f562d1c71b7a40d3cc77574598c44250920f70d
Your Name <you@example.com>
2021-05-03 14:58:49 +0800

    I added login method and i'm happy that my classmate teach me how to return in first execution hahahahha

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void loginMethod()
+        {
+            string username, password;
+            int ctr = 0;
+            Console.Write("\n\nCheck username and password :\n");
+            Console.Write(": Default user name and password is :abcd and 1234\n");
+            Console.Write("------------------------------------------------------\n");
+
+            do
+            {
+                Console.Write("Input a username: ");
+                username = Console.ReadLine();
+
+                Console.Write("Input a password: ");
+                password = Console.ReadLine();
+
+                if (username != "abcd" || password != "1234")
+                {
+                    ctr++;
+                }
+                else
+                {
+                    ctr = 1;
+
+                }
+            }
+            while ((username != "abcd" || password != "1234") && (ctr != 3));
+
+            if (ctr == 3)
+            {
+                Console.Write("\nLogin attemp three or more times. Try later!\n\n");
+                Console.WriteLine("Try again in 5 seconds.");
+                for (int i = 5; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    Console.WriteLine("Confirming...");
+
+                    Thread.Sleep(4000);
+                }
+                Console.Write("\nThe password entered successfully!\n\n");
+                Console.Write("------------------------------------------------------\n");
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,110 +1,316 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApp1
 {
-    class Program
+    class login
     {
-        static void Main(string[] args)
+        public void loginMethod()
         {
+            string username, password;
+            int ctr = 0;
+            Console.Write("\n\nCheck username and password :\n");
+            Console.Write(": Default user name and password is :abcd and 1234\n");
+            Console.Write("------------------------------------------------------\n");
 
+            do
+            {
+                Console.Write("Input a username: ");
+                username = Console.ReadLine();
 
+                Console.Write("Input a password: ");
+                password = Console.ReadLine();
+
+                if (username != "abcd" || password != "1234")
+                {
+                    ctr++;
+                }
+                else
+                {
+                    ctr = 1;
+
+                }
+            }
+            while ((username != "abcd" || password != "1234") && (ctr != 3));
+
+            if (ctr == 3)
+            {
+                Console.Write("\nLogin attemp three or more times. Try later!\n\n");
+                Console.WriteLine("Try again in 5 seconds.");
+                for (int i = 5; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+
+                }
+                loginMethod();
+            }
+            else
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Please Wait");
+                    Console.WriteLine("Confirming...");
+
+                    Thread.Sleep(4000);
+                }
+                Console.Write("\nThe password entered successfully!\n\n");
+                Console.Write("------------------------------------------------------\n");
+            }
+        }
+    }
+    class shopping
+    {
+        public void onlineShopping()
+        {
             string productName = "Ballpen";
             string productCategory = "School Supplies";
-            DateTime addedDate = new DateTime(2021, 04, 06);
-            int stockLimit = 500;
             int productQuantity = 500;
 
 
             string productName1 = "Shorts";
             string productCategory1 = "Trousers and Shorts";
-            DateTime addedDate1 = new DateTime(2021, 04, 06);
-            int stockLimit1 = 300;
             int productQuantity1 = 259;
 
 
             string productName2 = "Tshirt";
             string productCategory2 = "Shirts";
-            DateTime addedDate2 = new DateTime(2021, 04, 06);
-            int stockLimit2 = 300;
             int productQuantity2 = 201;
 
 
             string productName3 = "Bucket hat";
             string productCategory3 = "Cap";
-            DateTime addedDate3 = new DateTime(2021, 04, 06);
-            int stockLimit3 = 100;
-            int productQuantity3 = 78;
+            int productQuantity3 = 178;
 
+            Console.WriteLine();
             Console.WriteLine("Welcome to Online Shopping System!");
-            Console.WriteLine("Available Product: 1." + productName);
-            Console.WriteLine("Available Product: 2." + productName1);
-            Console.WriteLine("Available Product: 3." + productName2);
-            Console.WriteLine("Available Product: 4." + productName3);
+            Console.WriteLine("Available Product:");
+            Console.WriteLine("1." + productName);
+            Console.WriteLine("2." + productName1);
+            Console.WriteLine("3." + productName2);
+            Console.WriteLine("4." + productName3);
+            Console.WriteLine("");
+            Console.WriteLine("Press 5 to exit.");
+            Console.WriteLine("");
+            Console.WriteLine("");
 
             Console.Write("what product you want to buy: ");
             Console.WriteLine("Product: 1, 2, 3, or 4?");
+            
             int optionSelected = Convert.ToInt32(Console.ReadLine());
-            try {
-                switch (optionSelected)
-                {
-                    case 1: Console.WriteLine(productName); break;
-                    case 2: Console.WriteLine(productName1); break;
-                    case 3: Console.WriteLine(productName2); break;
-                    case 4: Console.WriteLine(productName3); break;
-                    default: Console.WriteLine("invalid option"); break;
-                }
 
-            } catch {
-                Console.Write("Type q if you want to view quantity or b if you want to buy: ");
-                string optionSelected1 = Console.ReadLine().ToLower();
-                if (optionSelected.Equals("q"))
-                {
-
-                    Console.WriteLine(productQuantity);
-                }
-                else if (optionSelected1.Equals("2"))
-                {
-                    Console.WriteLine(productQuantity1);
-                }
-                else if (optionSelected1.Equals("3"))
-                {
-                    Console.WriteLine(productQuantity2);
-                }
-                else if (optionSelected1.Equals("3"))
-                {
-                    Console.WriteLine(productQuantity3);
-                }
-                else
-                {
-                    Console.WriteLine("Error: Invalid option");
-
-                }
-
-
-            }
-            finally { 
-                
-                else if (optionSelected.Equals("b"))
-                {
+            switch (optionSelected)
+            {
+                case 1:
+                    Console.WriteLine("Product:" + productName);
+                    Console.WriteLine("Stock:" + productQuantity);
                     Console.WriteLine("How many would you like to buy?");
                     int quantity = Convert.ToInt32(Console.ReadLine());
-                    productQuantity -= quantity;
-                    //productQuantity = productQuantity - quantity
-                }
-                else
-                {
-                    Console.WriteLine("Error: Invalid option");
-                } /*
-                    Console.WriteLine("what item you want to view. Type 1 for ballpen, Type 2 for shorts, Type 3 for tshirt, Type 4 for hats");
+                    Console.WriteLine("you like to buy " + quantity, "?");
+                    Console.WriteLine(" Type Y to confirm then N to cancel");
                     string optionSelected1 = Console.ReadLine().ToLower();
-                    if (optionSelected.Equals("1"))
-                    {*/
+                    if (optionSelected1.Equals("y"))
+                    {
+                        int newproductQuantity = productQuantity - quantity;
+                        Console.WriteLine("You buy " + quantity);
+                        Console.WriteLine("Remaining stock is " + newproductQuantity);
+                    }
+                    else if (optionSelected1.Equals("n"))
+                    {
+                        Console.WriteLine("");
+                        onlineShopping();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Option!");
+                        Console.WriteLine("Restarting...");
+                        for (int i = 5; i > 0; i--)
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(1000);
+
+                        }
+                        onlineShopping();
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Product:" + productName1);
+                    Console.WriteLine("Stock:" + productQuantity1);
+                    Console.WriteLine("How many would you like to buy?");
+                    int quantity1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("you like to buy " + quantity1, "?");
+                    Console.WriteLine(" Type Y to confirm then N to cancel");
+                    string optionSelected2 = Console.ReadLine().ToLower();
+                    if (optionSelected2.Equals("y"))
+                    {
+                        int newproductQuantity = productQuantity - quantity1;
+                        Console.WriteLine("You buy" + quantity1);
+                        Console.WriteLine("Remaining stock is " + newproductQuantity);
+                    }
+                    else if (optionSelected2.Equals("n"))
+                    {
+                        Console.WriteLine("");
+                        onlineShopping();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Option!");
+                        Console.WriteLine("Restarting...");
+                        for (int i = 5; i > 0; i--)
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(1000);
+
+                        }
+                        onlineShopping();
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Product:" + productName2);
+                    Console.WriteLine("Stock:" + productQuantity2);
+                    Console.WriteLine("How many would you like to buy?");
+                    int quantity2 = Convert.ToInt32(Console.ReadLine());
+                    productQuantity2 -= quantity2;
+                    Console.WriteLine("you like to buy " + quantity2, "?");
+                    Console.WriteLine(" Type Y to confirm then N to cancel");
+                    string optionSelected3 = Console.ReadLine().ToLower();
+                    if (optionSelected3.Equals("y"))
+                    {
+                        int newproductQuantity = productQuantity - quantity2;
+                        Console.WriteLine("You buy" + quantity2);
+                        Console.WriteLine("Remaining stock is " + newproductQuantity);
+                    }
+                    else if (optionSelected3.Equals("n"))
+                    {
+                        Console.WriteLine("");
+                        onlineShopping();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Option!");
+                        Console.WriteLine("Restarting...");
+                        for (int i = 5; i > 0; i--)
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(1000);
+
+                        }
+                        onlineShopping();
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("Product:" + productName3);
+                    Console.WriteLine("Stock:" + productQuantity3);
+                    Console.WriteLine("How many would you like to buy?");
+                    int quantity3 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("you like to buy " + quantity3, "?");
+                    Console.WriteLine(" Type Y to confirm then N to cancel");
+                    string optionSelected4 = Console.ReadLine().ToLower();
+                    if (optionSelected4.Equals("y"))
+                    {
+                        int newproductQuantity = productQuantity - quantity3;
+                        Console.WriteLine("You buy" + quantity3);
+                        Console.WriteLine("Remaining stock is " + newproductQuantity);
+                    }
+                    else if (optionSelected4.Equals("n"))
+                    {
+                        Console.WriteLine("");
+                        onlineShopping();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Option!");
+                        Console.WriteLine("Restarting...");
+                        for (int i = 5; i > 0; i--)
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(1000);
+
+                        }
+                        onlineShopping();
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("Are you sure you want to exit?");
+                    Console.WriteLine("Press y if yes then n if no");
+                    string optionSelected5 = Console.ReadLine().ToLower();
+                    if (optionSelected5.Equals("y"))
+                    {
+
+                        Console.WriteLine("Thank you! Please come again!");
+                        Console.WriteLine("Closing...");
+                        for (int i = 5; i > 0; i--)
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(1000);
+
+                        }
+                    }
+                    else if (optionSelected5.Equals("n"))
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Restarting...");
+                        for (int i = 5; i > 0; i--)
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(1000);
+
+                        }
+                        onlineShopping();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Option!");
+                    }
+                    break;
+
+                    default: Console.WriteLine("invalid option");
+                    Console.WriteLine("Restarting...");
+                    for (int i = 5; i > 0; i--)
+                    {
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+
+                    }
+                    onlineShopping(); 
+                    break;
             }
+
 
 
 
         }
     }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            login up = new login();
+            up.loginMethod();
+
+            shopping os = new shopping();
+            os.onlineShopping();
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
 }
+    
+
+
+
 
 
